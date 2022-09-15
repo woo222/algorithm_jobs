ef1c0429ac5ddcca69689d783d20e6e9ac698bb4
wonjin <79980860+woo222@users.noreply.github.com>
2022-09-15 23:59:33 +0900

    Update and rename 출력예제.c to Part1 조건문&반복문.c

---
+++ Part1 조건문&반복문.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  // 1.출력
+  printf("Hello world!"); // printf - 출력
+  printf("My name is blahblahblah.\n"); // \n - 엔터
+  
+  // 2.변수
+  int a;
+  int b;
+  int c;
+  
+  a = 1;
+  b = 3;
+  c = 5;
+  
+  printf("%d %d %d\n", a, b, c); // 변수 출력
+  
+  
+  return 0; // 프로그램 정상종료
+}
--- 출력예제.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-  printf("Hello world!");
-
-  return 0; // 프로그램 정상종료
-}
