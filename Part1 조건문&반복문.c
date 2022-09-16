0c676ece9aaff86a321e5b4b6d347150e97bdff1
wonjin <79980860+woo222@users.noreply.github.com>
2022-09-16 22:38:19 +0900

    Update Part1 조건문&반복문.c
    캐스팅 학습

--- Part1 조건문&반복문.c
+++ Part1 조건문&반복문.c
@@ -25,6 +25,15 @@ int main() {
   printf("%d / %d : %d\n", first, second, first/second);
   printf("%d %% %d : %d\n", first, second, first%second); // 나머지, %를 출력하려면 %%
   
+  // 캐스팅
+  int sum = 1+2+3+4+5+6+7+8+9+10;
+  double average = (double)sum / 10; // 정수인 sum을 잠시 실수로 변경 -> 캐스팅
+  // double average = sum / 10.0;
+  
+  // 55 / 10 --> 5 정수와 정수 연산은 결과도 정수이다.
+  // 실수와 정수 사이 연산은 결과가 실수이다.
+  
+  printf("%lf\n", average); // 실수 하나 출력 lf
   
   
   return 0; // 프로그램 정상종료
