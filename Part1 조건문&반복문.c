a317796b21038fa09cc5eb0381aa4f5192fa4cdc
wonjin <79980860+woo222@users.noreply.github.com>
2022-09-16 11:41:53 +0900

    Update Part1 조건문&반복문.c

--- Part1 조건문&반복문.c
+++ Part1 조건문&반복문.c
@@ -5,16 +5,26 @@ int main() {
   printf("Hello world!"); // printf - 출력
   printf("My name is blahblahblah.\n"); // \n - 엔터
   
-  // 2.변수
-  int a;
-  int b;
-  int c;
   
-  a = 1;
-  b = 3;
-  c = 5;
+  // 2.변수
+  int a = 1;
+  int b = 3;
+  int c = 5;
   
   printf("%d %d %d\n", a, b, c); // 변수 출력
+  
+  // 사칙연산
+  int first, second;
+  
+  first = 4;
+  second = 2;
+  
+  printf("%d + %d : %d\n", first, second, first+second);
+  printf("%d - %d : %d\n", first, second, first-second);
+  printf("%d * %d : %d\n", first, second, first*second);
+  printf("%d / %d : %d\n", first, second, first/second);
+  printf("%d %% %d : %d\n", first, second, first%second); // 나머지, %를 출력하려면 %%
+  
   
   
   return 0; // 프로그램 정상종료
