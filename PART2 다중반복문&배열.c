1c9fdd2a21d19bd5a77cd72fde0af0e9a95ad3c1
wonjin <79980860+woo222@users.noreply.github.com>
2022-09-28 22:04:21 +0900

    Update PART2 다중반복문&배열.c
    배열에 값을 입력하고 출력하기

--- PART2 다중반복문&배열.c
+++ PART2 다중반복문&배열.c
@@ -1,11 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  
 /*
 * 1. 다중 반복문
 * nXn 별 그리기
 */
-#include <stdio.h>
-
-int main() {
-
   int n;
   scanf("%d", &n);
   
@@ -15,6 +15,25 @@ int main() {
     }
     printf("\n");
   }
+  
+
+/*
+* 2. 배열
+* 배열에 값을 입력하고 출력하기
+*/
+  int n;
+  int data[100];
+  
+  scanf("%d", &n);
+  
+  for(int i=0; i<n; i++) {
+    scanf("%d", &data[i]);
+  }
+  
+  for(int i=0; i<n; i++){
+    printf("%d ", data[i]);
+  }
+  
   
   return 0;
 }
